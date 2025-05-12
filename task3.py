90f0c586060fdf66b86c00d3458f545fbba4f3f6
KGlejkov <gklejkov@gmail.com>
2025-05-12 09:14:59 +0700

    Update task3.py

--- task3.py
+++ task3.py
@@ -6,3 +6,11 @@
 # Абракадабра
 # Вывод
 # а-5 б-2 д-1 к-1 р-2
+bukvi=input()
+proverki={}
+for i in range(len(bukvi)):
+    if bukvi[i] in proverki:
+        proverki[bukvi[i]]+=1
+    else:
+        proverki[bukvi[i]]=1
+print(proverki) 
